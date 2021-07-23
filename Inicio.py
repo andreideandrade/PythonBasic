be37a1008a6dae01b756e0e1b1f9ed1e61d1b608
ANDREI DE ANDRADE <eng.andreilino@gmail.com>
2021-07-22 21:19:43 -0300

    Operadores Condicionais Operadores de identidade

--- Inicio.py
+++ Inicio.py
@@ -44,3 +44,40 @@ if __name__ == '__main__':
     print("7 é maior ou igual a 3? -> ", 7 >= 3)
 
     print("3 é maior ou igual a 3? -> ", 3 >= 3)
+
+    ##############################
+    ##      Atividade 4         ##
+    ##############################
+    #   Operadores usando variáveis
+
+    x = 2
+    y = 7
+    z = 5
+    print("X é maior que Z? -> ", x > z)
+    print("Z é menor ou igual a y? -> ", z <= y)
+    print("Y é diferente de X? -> ", y != x)
+    print("==================================")
+    ##############################
+    ##      Atividade 4         ##
+    ##############################
+    #   Operadores usando condicionais
+
+    print("Operadores usando condicionais")
+    if 2 > 1:
+        print('Usando numeros: 2 é maior que 1')
+
+    num1 = 2
+    num2 = 1
+    if num1 > num2:
+        print('Usando variaveis: 2 é maior que 1')
+
+    ##############################
+    ##      Atividade 4         ##
+    ##############################
+    #   Operadores de identidade
+    print('Operadores de identidade')
+
+    aluguel = 250
+    energia = 250
+    agua = 65
+    print('Operador ( is ): ', aluguel is energia)
